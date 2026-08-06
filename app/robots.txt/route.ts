9b4cc9257cb63554769d41d25d22cf33764a7075
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-07 03:17:05 +0330

    feat(seo): add robots rules and sitemap reference

---
+++ app/robots.txt/route.ts
@@ -0,0 +1,28 @@
+import { absoluteUrl } from "../../lib/sitemap-xml";
+
+export const dynamic = "force-dynamic";
+
+export function GET() {
+  const body = [
+    "User-agent: *",
+    "Allow: /",
+    "Disallow: /admin/",
+    "Disallow: /account/",
+    "Disallow: /login",
+    "Disallow: /logout",
+    "Disallow: /auth/",
+    "Disallow: /api/",
+    "Disallow: /*?*returnTo=",
+    `Sitemap: ${absoluteUrl("/sitemap.xml")}`,
+    "",
+  ].join("\n");
+
+  return new Response(body, {
+    status: 200,
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+      "Cache-Control": "public, max-age=900, s-maxage=3600",
+      "X-Content-Type-Options": "nosniff",
+    },
+  });
+}

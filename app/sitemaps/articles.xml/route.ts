a66ffc1c934ecf0ca2aabcd43ac16cbe923b3fe8
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-07 16:10:45 +0330

    SEO: include published managed articles in sitemap

--- app/sitemaps/articles.xml/route.ts
+++ app/sitemaps/articles.xml/route.ts
@@ -1,9 +1,11 @@
-import { articles } from "../../articles/article-data";
+import { getPublishedArticles } from "../../../lib/content-articles";
 import { absoluteUrl, urlSet, xmlResponse } from "../../../lib/sitemap-xml";
 
 export const dynamic = "force-dynamic";
 
-export function GET() {
+export async function GET() {
+  const articles = await getPublishedArticles();
+
   return xmlResponse(
     urlSet([
       { loc: absoluteUrl("/articles"), changefreq: "weekly", priority: 0.7 },

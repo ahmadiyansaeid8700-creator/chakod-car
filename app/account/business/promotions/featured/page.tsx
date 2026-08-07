22a9f897412656e552a2ef9d50479f6fc9589ba5
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-07 13:19:01 +0330

    Add featured showroom booking page

---
+++ app/account/business/promotions/featured/page.tsx
@@ -0,0 +1,7 @@
+import FeaturedShowroomBookingClient from "./FeaturedShowroomBookingClient";
+
+export const dynamic = "force-dynamic";
+
+export default function FeaturedShowroomBookingPage() {
+  return <FeaturedShowroomBookingClient />;
+}

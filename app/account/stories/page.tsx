d042a301d835f381226c25edd63f785d1895defe
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-14 13:13:56 +0330

    Route stories to listing selector

--- app/account/stories/page.tsx
+++ app/account/stories/page.tsx
@@ -1,13 +1,5 @@
-import CommerceProductsPage from "../finance/CommerceProductsPage";
+import { redirect } from "next/navigation";
 
 export default function AccountStoriesPage() {
-  return (
-    <CommerceProductsPage
-      mode="promotions"
-      serviceKeys={["listing_story"]}
-      eyebrowOverride="CHAKOD STORIES"
-      titleOverride="استوری چاکود"
-      descriptionOverride="استوری را از داخل حساب مدیریت کنید؛ آگهی واقعی را انتخاب کنید و سفارش استوری را از همان مسیر حساب ادامه دهید."
-    />
-  );
+  redirect("/account/listings?intent=story");
 }

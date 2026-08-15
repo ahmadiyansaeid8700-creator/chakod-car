6868744a1ac9a5428a29814ed23c83e3da83a8e1
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-15 19:56:15 +0330

    feat: route selected page to six homepage placements

--- app/account/selected/page.tsx
+++ app/account/selected/page.tsx
@@ -1,13 +1,5 @@
-import CommerceProductsPage from "../finance/CommerceProductsPage";
+import SelectedPlacementClient from "./SelectedPlacementClient";
 
 export default function AccountSelectedPage() {
-  return (
-    <CommerceProductsPage
-      mode="promotions"
-      serviceKeys={["business_placement", "dealership_placement"]}
-      eyebrowOverride="CHAKOD SELECTED"
-      titleOverride="رزرو منتخب چاکود"
-      descriptionOverride="جایگاه منتخب نمایشگاه یا کسب‌وکار را از داخل حساب انتخاب کنید و رزرو را بدون خروج از پنل ادامه دهید."
-    />
-  );
+  return <SelectedPlacementClient />;
 }

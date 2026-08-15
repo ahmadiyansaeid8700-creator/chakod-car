8f40f342e90af331df86b1c6cd6c5fa9d75a1436
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-16 00:03:19 +0330

    refactor: isolate dealership directory experience

--- app/dealerships/page.tsx
+++ app/dealerships/page.tsx
@@ -1,14 +1,5 @@
-import BusinessesPage from "../businesses/page";
+import DealerDirectoryClient from "./DealerDirectoryClient";
 
 export default function DealershipsPage() {
-  return (
-    <BusinessesPage
-      initialType="dealer"
-      basePath="/dealerships"
-      lockType
-      kicker="دایرکتوری نمایشگاه‌های چاکود"
-      title="نمایشگاه‌های خودرو را یک‌جا مقایسه کنید"
-      description="نمایشگاه‌های تأییدشده را براساس نام و شهر پیدا کنید و اطلاعات کامل هر مجموعه را ببینید."
-    />
-  );
+  return <DealerDirectoryClient />;
 }

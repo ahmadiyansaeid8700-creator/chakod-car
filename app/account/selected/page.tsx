782ad2bf843941d77de8171105b3c0691ef020ee
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-15 22:07:56 +0330

    feat: enhance selected banner uploads on mobile

--- app/account/selected/page.tsx
+++ app/account/selected/page.tsx
@@ -1,10 +1,13 @@
+import BannerUploadEnhancer from "./BannerUploadEnhancer";
 import SelectedPlacementClient from "./SelectedPlacementClient";
 import ux from "./ux.module.css";
 
 export default function AccountSelectedPage() {
   return (
     <div className={ux.scope}>
-      <SelectedPlacementClient />
+      <BannerUploadEnhancer>
+        <SelectedPlacementClient />
+      </BannerUploadEnhancer>
     </div>
   );
 }

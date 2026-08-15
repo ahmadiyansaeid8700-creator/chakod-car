0c48cdc2e7fb8c904daea32414a8115b31cc845b
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-15 21:25:25 +0330

    style: apply simplified selected showroom flow

--- app/account/selected/page.tsx
+++ app/account/selected/page.tsx
@@ -1,5 +1,10 @@
 import SelectedPlacementClient from "./SelectedPlacementClient";
+import ux from "./ux.module.css";
 
 export default function AccountSelectedPage() {
-  return <SelectedPlacementClient />;
+  return (
+    <div className={ux.scope}>
+      <SelectedPlacementClient />
+    </div>
+  );
 }

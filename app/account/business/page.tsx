5573ce15d9304d343bc71729f323fe19566076ef
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-16 17:53:25 +0330

    Mount selected placement card in dealer overview

--- app/account/business/page.tsx
+++ app/account/business/page.tsx
@@ -1,7 +1,13 @@
 import DealerCommandCenter from "./DealerCommandCenter";
+import DealerSelectedPromotionInjector from "./DealerSelectedPromotionInjector";
 
 export const dynamic = "force-dynamic";
 
 export default function BusinessCommandPage() {
-  return <DealerCommandCenter />;
+  return (
+    <>
+      <DealerCommandCenter />
+      <DealerSelectedPromotionInjector />
+    </>
+  );
 }

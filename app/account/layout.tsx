047e61de4168f66b07baaf6902dfa1897acdd997
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-17 18:02:03 +0330

    Remove finance context banner from account pages

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,16 +1,9 @@
 import type { ReactNode } from "react";
 
-import AccountFinanceContext from "./AccountFinanceContext";
-
 type AccountLayoutProps = {
   children: ReactNode;
 };
 
 export default function AccountLayout({ children }: AccountLayoutProps) {
-  return (
-    <>
-      <AccountFinanceContext />
-      {children}
-    </>
-  );
+  return children;
 }

3c44fe530e001b0566f2b022c7982c699fecf3fc
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-17 22:25:20 +0330

    Remove injected duplicate business wallet

--- app/account-v2/businesses/[id]/layout.tsx
+++ app/account-v2/businesses/[id]/layout.tsx
@@ -1,12 +1,5 @@
 import type { ReactNode } from "react";
 
-import BusinessWalletCardInjector from "./BusinessWalletCardInjector";
-
 export default function BusinessActivityLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      {children}
-      <BusinessWalletCardInjector />
-    </>
-  );
+  return children;
 }

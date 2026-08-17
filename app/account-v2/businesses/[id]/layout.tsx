762cc87377f6ec17b79e1f46b2c260b4941533f6
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-17 23:02:53 +0330

    Mount exclusive business panel switcher

--- app/account-v2/businesses/[id]/layout.tsx
+++ app/account-v2/businesses/[id]/layout.tsx
@@ -1,5 +1,12 @@
 import type { ReactNode } from "react";
 
+import BusinessPanelSwitcher from "./BusinessPanelSwitcher";
+
 export default function BusinessActivityLayout({ children }: { children: ReactNode }) {
-  return children;
+  return (
+    <>
+      {children}
+      <BusinessPanelSwitcher />
+    </>
+  );
 }

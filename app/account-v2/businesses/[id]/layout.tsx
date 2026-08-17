f4c37a58f8f19e17a488d5e50cf0b1ec167f006d
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-17 23:10:31 +0330

    Remove legacy business panel switcher

--- app/account-v2/businesses/[id]/layout.tsx
+++ app/account-v2/businesses/[id]/layout.tsx
@@ -1,12 +1,5 @@
 import type { ReactNode } from "react";
 
-import BusinessPanelSwitcher from "./BusinessPanelSwitcher";
-
 export default function BusinessActivityLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      {children}
-      <BusinessPanelSwitcher />
-    </>
-  );
+  return children;
 }

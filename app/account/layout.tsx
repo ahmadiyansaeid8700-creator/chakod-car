234da5f12302d865427c6c8965fd615d8628553e
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-17 12:27:28 +0330

    Sync finance scope in account layout

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,9 +1,16 @@
 import type { ReactNode } from "react";
 
+import AccountFinanceContext from "./AccountFinanceContext";
+
 type AccountLayoutProps = {
   children: ReactNode;
 };
 
 export default function AccountLayout({ children }: AccountLayoutProps) {
-  return children;
+  return (
+    <>
+      <AccountFinanceContext />
+      {children}
+    </>
+  );
 }

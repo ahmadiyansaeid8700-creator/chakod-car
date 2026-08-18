d7ea60263ab46d698c57eefef0011f7061559d1e
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-18 21:27:32 +0330

    Register SEO article collection

---
+++ app/articles/seo/index.ts
@@ -0,0 +1,24 @@
+import type { Article } from "../article-data";
+import { usedCarPricingGuide } from "./used-car-pricing-guide";
+import { carInspectionBeforeBuying } from "./car-inspection-before-buying";
+import { usedCarDocumentsChecklist } from "./used-car-documents-checklist";
+import { howToCreateCarListing } from "./how-to-create-car-listing";
+import { freeZoneCarBuyingGuide } from "./free-zone-car-buying-guide";
+import { usedLuxuryCarBuyingGuide } from "./used-luxury-car-buying-guide";
+import { checkRealCarMileage } from "./check-real-car-mileage";
+import { hiddenCostsUsedCar } from "./hidden-costs-used-car";
+import { howToCompareCarsBeforeBuying } from "./how-to-compare-cars-before-buying";
+import { sellCarFasterGuide } from "./sell-car-faster-guide";
+
+export const seoArticles: Article[] = [
+  usedCarPricingGuide,
+  carInspectionBeforeBuying,
+  usedCarDocumentsChecklist,
+  howToCreateCarListing,
+  freeZoneCarBuyingGuide,
+  usedLuxuryCarBuyingGuide,
+  checkRealCarMileage,
+  hiddenCostsUsedCar,
+  howToCompareCarsBeforeBuying,
+  sellCarFasterGuide,
+];

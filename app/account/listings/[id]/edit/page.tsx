3abb01e3ea452c256ec5443bf0798a32b9e88f1b
ahmadiyansaeid8700-creator <ahmadiyan.saeid8700@gmail.com>
2026-08-18 19:03:20 +0330

    Remove duplicate inline photo manager

--- app/account/listings/[id]/edit/page.tsx
+++ app/account/listings/[id]/edit/page.tsx
@@ -1,5 +1,4 @@
 import ListingEditClient from "./ListingEditClient";
-import InlineListingImagesManager from "./InlineListingImagesManager";
 
 export default async function AccountListingEditPage({
   params,
@@ -7,10 +6,5 @@ export default async function AccountListingEditPage({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  return (
-    <>
-      <ListingEditClient listingId={id} />
-      <InlineListingImagesManager listingId={id} />
-    </>
-  );
+  return <ListingEditClient listingId={id} />;
 }
